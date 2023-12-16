653873fc915a2c415e53107c89e170496bfd2f63
XSaaM <151796109+XSaaM@users.noreply.github.com>
2023-12-16 13:01:41 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+setTimeout(cambiaMensaje,2500)
+
+function cambiaMensaje(){
+    document.getElementById("loading-messager").innerText = "Enviado!";
+}
+
+setTimeout(function (){
+    alert("Resiviste un abrazo!")
+
+},3000)
+
+
